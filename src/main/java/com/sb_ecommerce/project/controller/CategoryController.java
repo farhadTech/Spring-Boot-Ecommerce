5b8d6baaa5616d5656c7068b5a4c1945dd1f985c
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-02-23 12:20:09 +0600

    "using ResponseEntity"

--- src/main/java/com/sb_ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/sb_ecommerce/project/controller/CategoryController.java
@@ -3,7 +3,10 @@ package com.sb_ecommerce.project.controller;
 // Import necessary classes
 import com.sb_ecommerce.project.model.Category;
 import com.sb_ecommerce.project.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -48,8 +51,12 @@ public class CategoryController {
      */
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId) {
-        String status = categoryService.deleteCategory(categoryId);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch(ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 }
