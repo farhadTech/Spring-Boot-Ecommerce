0080566ea684e680ecae03c0ba5c7cc332cce56d
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-02-24 07:19:14 +0600

    "adding updateCategory logic"

--- src/main/java/com/sb_ecommerce/project/service/CategoryService.java
+++ src/main/java/com/sb_ecommerce/project/service/CategoryService.java
@@ -10,6 +10,8 @@ public interface CategoryService {
     void createCategory(Category category);
 
     String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category, Long categoryId);
 }
 
 /*
