346c7e99ceda5af592669d8f6a4ad6fca3580964
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-02-25 20:14:16 +0600

    "validation for no categories present"

--- src/main/java/com/sb_ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/sb_ecommerce/project/service/CategoryServiceImpl.java
@@ -25,7 +25,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List <Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty())
+            throw new APIException("No Category created till now");
+        return categories;
     }
 
     @Override
