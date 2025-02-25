aa17bc06605f9d6795700951b5f8aa3f334f7d0a
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-02-25 10:22:00 +0600

    "added lombok"

--- src/main/java/com/sb_ecommerce/project/model/Category.java
+++ src/main/java/com/sb_ecommerce/project/model/Category.java
@@ -4,29 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "Categories") // changing table name
 public class Category {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public Category() {}
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-    public String getCategoryName() {
-        return categoryName;
-    }
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-    public Long getCategoryId() {
-        return categoryId;
-    }
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
 }
