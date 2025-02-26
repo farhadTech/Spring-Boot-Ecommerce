b3c9ed34cc4929ccf4f784201abf2fde97a4c10c
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-02-26 20:54:52 +0600

    "@RequestParam annotation"

--- src/main/java/com/sb_ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/sb_ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.sb_ecommerce.project.controller;
 
-import com.sb_ecommerce.project.model.Category;
 import com.sb_ecommerce.project.payload.CategoryDTO;
 import com.sb_ecommerce.project.payload.CategoryResponse;
 import com.sb_ecommerce.project.service.CategoryService;
@@ -10,14 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/echo")
+    public ResponseEntity<String> echoMessage(@RequestParam(name="message", defaultValue = "Hello World!") String message) {
+        return new ResponseEntity<>("Echoed message: " + message, HttpStatus.OK);
+    }
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories() {
